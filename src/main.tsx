b786d5b6a11aaacb37b490caa9bbb2ca17d320e5
kimjiho1125 <kimjiho1125a@naver.com>
2025-04-01 23:39:45 +0900

    [Set] Sentry 설정 및 포로덕션에서만 활성화

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import * as Sentry from "@sentry/react";
 
+if (import.meta.env.MODE === "production" && import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0,
+    release: "my-app@1.0.0",
+    environment: "production",
+  });
+}
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
