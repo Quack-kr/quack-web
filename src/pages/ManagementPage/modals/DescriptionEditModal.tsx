d07598ba4b19180b07c71f0143cb783113f35ee1
kimjiho1125 <kimjiho1125a@naver.com>
2025-04-01 21:50:41 +0900

    [style] relative units -> absolute로 수정

--- src/pages/ManagementPage/modals/DescriptionEditModal.tsx
+++ src/pages/ManagementPage/modals/DescriptionEditModal.tsx
@@ -57,8 +57,8 @@ const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   background: rgba(0, 0, 0, 0.6);
   display: flex;
   align-items: center;
