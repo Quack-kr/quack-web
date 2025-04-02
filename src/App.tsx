b7a47fe3a96e2f9958e58b775cc0b8c2d28bb44f
kimjiho1125 <kimjiho1125a@naver.com>
2025-04-02 23:25:31 +0900

    [style] 화면 가운데 정렬되도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -14,19 +14,38 @@ function App() {
   return (
     <Router>
       <GlobalStyles />
-      <Header />
-      {/* 라우터를 정의하는 Routes */ }
-      <Routes>
-        <Route path="/" element={<HomePage />} /> {/* 기본 홈 */}
-        <Route path="/management" element={<ManagementPage />} /> {/* 사장님 가게관리 페이지 */}
-        <Route path="/contact" element={<ContactPage />} /> {/* 문의하기 페이지 */}
-        <Route path="/management/signin" element={<LoginPage />}  /> {/* 로그인 페이지 */}
-        <Route path="/management/signup" element={<SignUpPage/>} /> {/* 회원가입 페이지 */}
-        <Route path="/management/apply" element={<ApplyPage/>} /> {/* 온라인 입점신청 페이지 */}
-      </Routes>
-      <Footer />
+      <div style={styles.pageContainer}>
+        <Header />
+        {/* 라우터를 정의하는 Routes */}
+        <Routes>
+          <Route path="/" element={<HomePage />} /> {/* 기본 홈 */}
+          <Route path="/management" element={<ManagementPage />} />
+          {/* 사장님 가게관리 페이지 */}
+          <Route path="/contact" element={<ContactPage />} />
+          {/* 문의하기 페이지 */}
+          <Route path="/management/signin" element={<LoginPage />} />
+          {/* 로그인 페이지 */}
+          <Route path="/management/signup" element={<SignUpPage />} />
+          {/* 회원가입 페이지 */}
+          <Route path="/management/apply" element={<ApplyPage />} />
+          {/* 온라인 입점신청 페이지 */}
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
+
+// 중앙 정렬 스타일 (React.CSSProperties 타입으로 명시)
+const styles: { pageContainer: React.CSSProperties } = {
+  pageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    //justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh', // 화면의 최소 높이를 100%로 설정하여 모든 콘텐츠가 화면에 맞게 중앙에 위치하도록 함
+    paddingBottom: '50px', // Footer와의 간격을 유지하기 위해 하단 패딩 추가
+  }
+};
 
 export default App;
