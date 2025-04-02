211ad626d50678e0f5b01ad71a35bc2b90922c7c
kimjiho1125 <kimjiho1125a@naver.com>
2025-04-02 22:32:20 +0900

    [set] Sentry 설정값 변경

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ if (import.meta.env.MODE === "production" && import.meta.env.VITE_SENTRY_DSN) {
     dsn: import.meta.env.VITE_SENTRY_DSN,
     integrations: [Sentry.browserTracingIntegration()],
     tracesSampleRate: 1.0,
-    release: "my-app@1.0.0",
+    release: "quack@1.0.0",
     environment: "production",
   });
 }
