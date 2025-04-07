444079b4b4af8fdac670488bf0f9f32767290099
aihoshistar <troublesome.dev@gmail.com>
2025-04-07 22:57:08 +0900

    [Feat] 이미지 경로 수정

--- src/pages/MobileHomePage/MobileHomePage.tsx
+++ src/pages/MobileHomePage/MobileHomePage.tsx
@@ -34,83 +34,9 @@ const MobileHomePage: React.FC = () => {
 export default MobileHomePage;
 
 const MobileHomePageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0;
-  width: 1920px;
-  margin-top: 50px;
-  background-color: #f5c622;
+  width: 100%;
+  max-width: 540px;
+  background-color: #ffc6ff;
   font-family: TheJamsil5;
   overflow: hidden;
-`;
-
-const Section = styled.section<{ isRegister?: boolean; height?: string }>`
-  display: flex;
-  flex-direction: column;
-  align-items: ${({ isRegister }) => (isRegister ? "center" : "flex-start")};
-  background-color: #f5c622;
-  width: 1920px;
-  padding: 200px 335px 0px 335px;
-  height: ${({ height }) => height || "980px"};
-  overflow: hidden;
-`;
-
-const Title1 = styled.div`
-  font-size: 80px;
-  line-height: 100px;
-  font-weight: 700;
-  color: #070706;
-  margin-bottom: 20px;
-
-  @media (max-width: 768px) {
-    font-size: 32px;
-    font-weight: 700;
-  }
-`;
-const Title2 = styled.div`
-  font-size: 64px;
-  line-height: 80px;
-  font-weight: 700;
-  color: #070706;
-  margin-bottom: 20px;
-`;
-const Text = styled.div`
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 26px;
-  color: #070706;
-  font-family: Pretendard;
-`;
-
-const RegisterBtn = styled.div`
-  width: 200px;
-  height: 72px;
-  margin-top: 100px;
-  margin-bottom: 20px;
-  padding: 32px 16px;
-  border-radius: 40px;
-  font-weight: 700;
-  font-size: 24px;
-  color: #efeedf;
-  background-color: #070706;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const Image = styled.img<{
-  width?: string;
-  height?: string;
-  translateX?: string;
-  translateY?: string;
-  marginTop?: string;
-}>`
-  width: ${({ width }) => width || "auto"};
-  height: ${({ height }) => height || "auto"};
-  transform: translate(
-    ${({ translateX }) => translateX || "0"},
-    ${({ translateY }) => translateY || "0"}
-  );
-  margin-top: ${({ marginTop }) => marginTop || "20px"};
 `;
