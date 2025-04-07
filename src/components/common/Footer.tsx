5e13e43f71c70fe0a0564cbf3da275e9ca64dc49
kimjiho1125 <kimjiho1125a@naver.com>
2025-04-07 22:01:37 +0900

    [style] Footer 수정사항 반영

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -74,13 +74,12 @@ const Footer: React.FC = () => (
     <FooterContainer>
       <Logo src={MainLogo} />
       <CompanyInformationContainer>
-        <Title>꽥 플레이스</Title>
-        <Text>사업자 등록번호: 721-04-03645</Text>
+        <Text>상호명: 꽥 | 사업자 등록번호: 721-04-03645 | 대표자: 김걸휘</Text>
         <Text>
           주소: 경기도 남양주시 와부읍 수레로116번길 16,
           402호-J186호(아이비타워-2)
         </Text>
-        <Text>문의사항: quack0410@naver.com</Text>
+        <Text>문의사항: quack0410@naver.com | 010-4121-9833</Text>
       </CompanyInformationContainer>
       <CopyRightContainer>
         <Title>개인정보처리방침 | 꽥 서비스 이용약관</Title>
